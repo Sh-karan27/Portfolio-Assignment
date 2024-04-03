c20e00e241cb8f0e73a1800360369fdbe73426cb
Sh-karan27 <shuklakaran107@gmail.com>
2024-04-03 13:03:12 +0530

    app.jsx chnage

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import Home from './Components/Home';
-import Work from './Components/Work';
-import About from './Components/About';
-import Contact from './Components/Contact';
-import Services from './Components/Services';
-import Footer from './Components/Footer';
-import Error from './Components/Error';
+import React from 'react'
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/services' element={<Services />} />
-        <Route path='/project' element={<Work />} />
-        <Route path='/contact' element={<Contact />} />
-        <Route path='*' element={<Error />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
-};
+    <div>App</div>
+  )
+}
 
-export default App;
+export default App
